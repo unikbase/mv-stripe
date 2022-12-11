7efcba97cf02dc24d08c0f91b375086217d67f08
meveo.admin <no-mail@meveo.com>
2022-12-11 04:20:07 +0000

    Add the script File : org.meveo.stripe.CheckoutSessionScriptin the module : mv-stripe

--- facets/java/org/meveo/stripe/CheckoutSessionScript.java
+++ facets/java/org/meveo/stripe/CheckoutSessionScript.java
@@ -5,6 +5,10 @@ import java.util.Map;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
 
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+
 public class CheckoutSessionScript extends Script {
 	
 	@Override
